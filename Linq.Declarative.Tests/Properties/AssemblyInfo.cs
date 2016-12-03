47dfac7653e099d8d6cd3b51e2ea2f0e8ed38d33
Ignacio Garay <imgarayojeda@gmail.com>
2016-12-03 15:10:19 -0300

    Fix to AssemblyInfo & switched to net 4.6 before changing to core

--- Linq.Declarative.Tests/Properties/AssemblyInfo.cs
+++ Linq.Declarative.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Trunks.LinqExtensions.Tests")]
+[assembly: AssemblyTitle("Linq.Declarative.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Trunks.LinqExtensions.Tests")]
+[assembly: AssemblyProduct("Linq.Declarative.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
